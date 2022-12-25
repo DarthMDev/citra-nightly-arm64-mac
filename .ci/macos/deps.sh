dd8ad34663f7cae9647a060eef5206ad55a86b92
DarthNihilus1 <mgracer48@yahoo.com>
2022-12-25 17:11:34 -0600

    deps: Manually compile sdl2 for universal2
    Macports failed to provide a universal2 build for some reason

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -10,7 +10,7 @@ sudo cp -rv FFmpeg-shared-n5.0.1-OSX-universal/* /usr/local/
 # copy to /Users/runner/work/FFmpeg-macOS/FFmpeg-macOS/ffmpeg/install_universal
 mkdir -p /Users/runner/work/FFmpeg-macOS/FFmpeg-macOS/ffmpeg/install_universal
 sudo cp -rv FFmpeg-shared-n5.0.1-OSX-universal/* /Users/runner/work/FFmpeg-macOS/FFmpeg-macOS/ffmpeg/install_universal
-sudo port install libsdl2 +universal openssl +universal openssl3 +universal
+sudo port install openssl +universal openssl3 +universal
 sudo port install moltenvk 
 # grab qt5 universal2 binaries
 wget https://github.com/MichaelGDev48/qt5.15.2-universal-binaries/releases/download/1.0/Qt-5.15.2-universal.zip 
@@ -32,7 +32,15 @@ export SDL_VER=2.0.16
 export FFMPEG_VER=4.4
 mkdir tmp
 cd tmp/
-
+wget https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.0.16.zip
+unzip release-2.0.16.zip -d sdl-release-2.0.16
+cd sdl-release-2.0.16/SDL-release-2.0.16/
+mkdir build
+cd build
+cmake .. "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
+cmake --build .
+sudo cmake --install .
+cd ../../..
 # install SDL
 # wget https://github.com/SachinVin/ext-macos-bin/raw/main/sdl2/sdl-${SDL_VER}.7z
 # 7z x sdl-${SDL_VER}.7z
