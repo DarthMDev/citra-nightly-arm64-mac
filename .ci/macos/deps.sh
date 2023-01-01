4ae3b3a2a18cb81aee68cd6b088d4d62830b75a1
DarthNihilus1 <mgracer48@yahoo.com>
2023-01-01 14:47:42 -0500

    deps: Go back to port since brew errors with python installation

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -4,8 +4,9 @@ rm '/usr/local/bin/2to3' || true
 brew update
 
 export PATH=$PATH:/opt/local/bin
-brew install ninja ccache p7zip
-wget https://github.com/ColorsWind/FFmpeg-macOS/releases/download/n5.0.1-patch3/FFmpeg-shared-n5.0.1-OSX-universal.zip
+wget https://github.com/macports/macports-base/releases/download/v2.8.0/MacPorts-2.8.0-12-Monterey.pkg
+sudo installer -pkg ./MacPorts-2.8.0-12-Monterey.pkg -target /
+sudo port install cmake ninja ccache p7zip
 unzip FFmpeg-shared-n5.0.1-OSX-universal.zip -d FFmpeg-shared-n5.0.1-OSX-universal
 sudo cp -rv FFmpeg-shared-n5.0.1-OSX-universal/* /usr/local/
 # copy to /Users/runner/work/FFmpeg-macOS/FFmpeg-macOS/ffmpeg/install_universal
