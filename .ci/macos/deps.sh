dd14f152398648f6984ebb78871667406711c282
DarthNihilus1 <mgracer48@yahoo.com>
2023-01-07 01:12:50 -0500

    Update deps.sh

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -7,8 +7,8 @@ brew update
 # wget https://github.com/macports/macports-base/releases/download/v2.8.0/MacPorts-2.8.0-12-Monterey.pkg
 # sudo installer -pkg ./MacPorts-2.8.0-12-Monterey.pkg -target /
 # brew fails otherwise if we dont uninstall python3 first (issue with existing ide)
-brew uninstall python3.10
-brew install p7zip ccache ninja python3.10 || true
+brew uninstall python@3.10
+brew install p7zip ccache ninja python@3.10 || true
 
 
 git clone https://github.com/MichaelGDev48/citra-dependencies-universal2
